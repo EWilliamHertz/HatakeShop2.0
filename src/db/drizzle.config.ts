f89716040db2fef701bd40983e5d0982d9946b38
EWilliamHertz <74967501+EWilliamHertz@users.noreply.github.com>
2026-09-12 21:50:29 +0200

    drizzzle fix

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -4,18 +4,18 @@ dotenv.config();
 
 export default defineConfig(process.env.DATABASE_URL ? {
   dialect: "postgresql",
-  schema: "./src/db/schema.js",
+  schema: "./src/db/schema.ts",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
 } : {
   dialect: "postgresql",
-  schema: "./src/db/schema.js",
+  schema: "./src/db/schema.ts",
   dbCredentials: {
     host: process.env.SQL_HOST || "",
     user: process.env.SQL_USER || "",
     password: process.env.SQL_PASSWORD || "",
     database: process.env.SQL_DB_NAME || "",
-    ssl: false,
+    ssl: true,
   },
 });
